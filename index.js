d2263bed0fa895113d88cffb67e3ab16b04403ec
AndreyTrolezi <laureano.yane@gmail.com>
2023-11-19 16:59:28 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express =require("express")
 
 const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("cr7 melhor do mundo")
+})
+
+app.listen(3000, () => {
+    console.log("server is online on 3000")
+})
