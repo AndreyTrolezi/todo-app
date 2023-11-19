8fc324ebfb6a30cf6d036b4343c064e62ca46d2c
AndreyTrolezi <laureano.yane@gmail.com>
2023-11-19 17:22:31 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.engine('handleabars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("cr7 melhor do mundo")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
