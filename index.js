815f2193cfb4bd925e56f6fce18fb4adad788f27
AndreyTrolezi <laureano.yane@gmail.com>
2023-11-21 21:25:22 -0300

    conexão com mysql finalizada

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ const conexao = mysql.createConnection({
     port: 3306
 })
 
+conexao.connect((error) => {
+    if (error) {
+        return console.log("access denided")
+    }
+
+    console.log("access guaranteed, connected on the servers of mysql")
+
+    app.listen(3000, () => {
+        console.log("connected to the residence of the loucos of north zone")
+    })
+})
